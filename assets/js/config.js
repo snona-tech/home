ae3cb7e49146387cbb7aafff1d763b3d8a28c79a
s-Nought <eulersconst0.577@gmail.com>
2020-10-17 03:42:09 +0900

    add talk

--- assets/js/config.js
+++ assets/js/config.js
@@ -12,6 +12,12 @@ const PORTFOLIO_LIST = [
         'text': 'A game exploring the truth with a loved one.'
     },
     {
+        'title': 'Talk Theme',
+        'url': 'https://s-nought.github.io/talk/',
+        'image_url': 'assets/img/portfolio/talk.jpg',
+        'text': 'Let\'s decide the talk theme'
+    },
+    {
         'title': 'No Name',
         'url': '',
         'image_url': 'assets/img/portfolio/comingsoon.jpg',
